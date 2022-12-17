bfe37ed296fb874f7bcd4eb8ffc74da4f0652563
JeffXiesk <12011206@mail.sustech.edu.cn>
2022-12-17 13:19:48 +0800

    no staggler test

--- deployment/scripts/experiment-configuration/generate-local-config.sh
+++ deployment/scripts/experiment-configuration/generate-local-config.sh
@@ -43,7 +43,7 @@ clients32=""    # deploys 32 client machine which run the specified number of cl
 systemSizes="4" # Must be sorted in ascending order!
 failureCounts=(0) # For each system size, the corresponding failure count (on top of the correct nodes)
 
-StragglerCnt=(1) # Count of Straggler (Only effect when crashTimings is 'Straggler')
+StragglerCnt=(0) # Count of Straggler (Only effect when crashTimings is 'Straggler')
 
 reuseFaulty=true  # If true, both correct and faulty peers will have the same tag and will be launched together, with the same config file.
                   # The failure count is only expressed as a parameter in (every peer's) config file, and even the faulty peers will see
