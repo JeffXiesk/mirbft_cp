0bc139db9e488d8101b610e50c30bf357d22edd3
Seok <75991596+JeffXiesk@users.noreply.github.com>
2022-12-19 02:56:45 +0000

    add test result

--- deployment/scripts/cloud-deploy/deploy_multiple.sh
+++ deployment/scripts/cloud-deploy/deploy_multiple.sh
@@ -4,7 +4,7 @@ len=${#bandwidth[@]}
 
 for ((j=1;j<=3;j++)); do
     for ((i=0; i < len; i++)); do
-        echo $j ${bandwidth[$i]} > scripts/cloud-deploy/execute_order.log
+        echo $j ${bandwidth[$i]} >> scripts/cloud-deploy/execute_order.log
         # echo "source scripts/cloud-deploy/deploy-cloud.sh -b $j ${bandwidth[$i]}"
         scripts/cloud-deploy/deploy-cloud.sh -b $j ${bandwidth[$i]} &
         echo '======'
