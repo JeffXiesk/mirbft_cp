66aa6eaeccbcc97f56f5e8a8b6aacc9fd697960d
JeffXiesk <12011206@mail.sustech.edu.cn>
2023-07-13 11:18:14 +0800

    Add mirbft breakdown

--- deployment/cloud-deploy/deploy.sh
+++ deployment/cloud-deploy/deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-# bash cloud-deploy/deploy-cloud-LAN.sh -i -n 32 8 -r -w -s -c 0 --wan --run
-=======
 # bash deploy-cloud-WAN.sh -i -n 32 8 -r -w -s -c 0 --wan --run
 # bash deploy-cloud-WAN.sh -i -n 1 1 -r -w -s -c 0 --wan --run
-
->>>>>>> ba5dd455505639ecd7048860219163cefd7082b1
 
 for i in {1..15}; do
     echo "$i"
